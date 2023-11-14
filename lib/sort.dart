f40b7217fd5975aaa4d6f8e3e3db1be8a67659da
Haloio29 <aureliolucio5@gmail.com>
2023-11-14 22:54:18 +0700

    change design UI

--- lib/sort.dart
+++ lib/sort.dart
@@ -10,7 +10,7 @@ class SortPages extends StatefulWidget {
 class _SortPagesState extends State<SortPages> {
   // variabel untuk form
   final formKey = GlobalKey<FormState>();
-  TextEditingController _textController = TextEditingController();
+  final _textController = TextEditingController();
   var list = [];
   var listBeforeSorting = [];
 
@@ -70,184 +70,219 @@ class _SortPagesState extends State<SortPages> {
         backgroundColor: Colors.blue,
       ),
       body: SingleChildScrollView(
-        child: Padding(
+        child: Container(
           padding: EdgeInsets.all(10),
-          child: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                // Bagian Input Data Ingin Di Sorting (TAHAP 1)
-                TextFormField(
-                  controller: _textController,
-                  validator: (value) => value == null || value.isEmpty
-                      ? 'Data nya masih kosong'
-                      : null,
-                  decoration: InputDecoration(
-                    labelText: "Masukan berapa data yang ingin di sorting",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                ButtonBar(
-                  alignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(
-                          () {
-                            if (_textController.text.length != 0) {
-                              int data =
-                                  int.parse(_textController.text.toString());
-                              list.add(data);
-                              listBeforeSorting.add(data);
-                              _textController.text = "";
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  backgroundColor: Colors.red,
-                                  content:
-                                      Text('Data inputan masih kosong !!!'),
-                                ),
-                              );
-                            }
-                          },
-                        );
-                      },
-                      child: Text("Input"),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(
-                          () {
-                            if (list.isEmpty) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  backgroundColor: Colors.red,
-                                  content:
-                                      Text('Tidak ada data yang tersedia !!!'),
-                                ),
-                              );
-                            } else {
-                              list.clear();
-                              listBeforeSorting.clear();
-                              _textController.clear();
-                              _textController.text = "";
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  backgroundColor: Colors.green,
-                                  content: Text('Data berhasil di hapus !!!'),
-                                ),
-                              );
-                            }
-                          },
-                        );
-                      },
-                      child: Text("Clear"),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 30,
-                  width: double.infinity,
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      'DATA SEBELUM DI SORTING',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
+          color: Colors.green,
+          child: Row(
+            children: [
+              Form(
+                  key: formKey,
+                  child: Container(
+                    width: 300,
+                    child: TextFormField(
+                      controller: _textController,
+                      validator: (value) => value == null || value.isEmpty
+                          ? 'Data nya masih kosong'
+                          : null,
+                      decoration: InputDecoration(
+                        labelText: "Masukan berapa data yang ingin di sorting",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                       ),
                     ),
+                  )),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Input"),
                   ),
-                  decoration: BoxDecoration(
-                    color: Colors.green[200],
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                    ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Input"),
                   ),
-                ),
-                // Bagian Input Data Yang Sudah Di tentukan di Atas (TAHAP 2)
-                Container(
-                  child: Text('${listBeforeSorting}'),
-                  padding: EdgeInsets.all(10),
-                  height: 130,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10)),
-                    color: Colors.grey[350],
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  width: double.infinity,
-                  child: FilledButton(
-                    onPressed: () {
-                      List<int> listBeforeSorting = List.from(list);
-                      setState(() {
-                        sorting(list);
-                        // Menampilkan list sebelum diurutkan
-                        debugPrint('Sebelum sorting: ${listBeforeSorting}');
-                        // Menampilkan list setelah diurutkan
-                        debugPrint('Setelah sorting: ${list}');
-                      });
-                    },
-                    child: Text('URUTKAN'),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 30,
-                  width: double.infinity,
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      'DATA SETELAH DI SORTING',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.green[200],
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                    ),
-                  ),
-                ),
-                // Bagian Data Yang Sudah Di Tentukan (TAHAP 3)
-                Container(
-                  child: Text('${list}'),
-                  padding: EdgeInsets.all(10),
-                  height: 130,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10)),
-                    color: Colors.grey[350],
-                  ),
-                ),
-              ],
-            ),
+                ],
+              )
+            ],
           ),
         ),
       ),
+      // body: SingleChildScrollView(
+      //   child: Padding(
+      //     padding: EdgeInsets.all(10),
+      //     child: Form(
+      //       key: formKey,
+      //       child: Column(
+      //         children: [
+      //           // Bagian Input Data Ingin Di Sorting (TAHAP 1)
+      //           TextFormField(
+      //             controller: _textController,
+      //             validator: (value) => value == null || value.isEmpty
+      //                 ? 'Data nya masih kosong'
+      //                 : null,
+      //             decoration: InputDecoration(
+      //               labelText: "Masukan berapa data yang ingin di sorting",
+      //               border: OutlineInputBorder(
+      //                 borderRadius: BorderRadius.circular(10),
+      //               ),
+      //             ),
+      //           ),
+      //           SizedBox(
+      //             height: 5,
+      //           ),
+      //           Row(
+      //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //             children: [
+      //               ElevatedButton(
+      //                 onPressed: () {
+      //                   setState(() {
+      //                     if (_textController.text.length != 0) {
+      //                       int data =
+      //                           int.parse(_textController.text.toString());
+      //                       list.add(data);
+      //                       listBeforeSorting.add(data);
+      //                       _textController.text = "";
+      //                     } else {
+      //                       ScaffoldMessenger.of(context).showSnackBar(
+      //                         SnackBar(
+      //                           backgroundColor: Colors.red,
+      //                           content: Text('Data inputan masih kosong !!!'),
+      //                         ),
+      //                       );
+      //                     }
+      //                   });
+      //                 },
+      //                 child: Text("Input"),
+      //               ),
+      //               ElevatedButton(
+      //                 onPressed: () {
+      //                   setState(() {
+      //                     if (list.isEmpty) {
+      //                       ScaffoldMessenger.of(context).showSnackBar(
+      //                         SnackBar(
+      //                           backgroundColor: Colors.red,
+      //                           content:
+      //                               Text('Tidak ada data yang tersedia !!!'),
+      //                         ),
+      //                       );
+      //                     } else {
+      //                       list.clear();
+      //                       listBeforeSorting.clear();
+      //                       _textController.clear();
+      //                       _textController.text = "";
+      //                       ScaffoldMessenger.of(context).showSnackBar(
+      //                         SnackBar(
+      //                           backgroundColor: Colors.green,
+      //                           content: Text('Data berhasil dihapus !!!'),
+      //                         ),
+      //                       );
+      //                     }
+      //                   });
+      //                 },
+      //                 child: Text("Clear"),
+      //               ),
+      //             ],
+      //           ),
+      //           SizedBox(
+      //             height: 10,
+      //           ),
+      //           Container(
+      //             height: 30,
+      //             width: double.infinity,
+      //             child: Align(
+      //               alignment: Alignment.center,
+      //               child: Text(
+      //                 'DATA SEBELUM DI SORTING',
+      //                 style: TextStyle(
+      //                   color: Colors.black,
+      //                   fontWeight: FontWeight.bold,
+      //                 ),
+      //               ),
+      //             ),
+      //             decoration: BoxDecoration(
+      //               color: Colors.green[200],
+      //               borderRadius: BorderRadius.only(
+      //                 topLeft: Radius.circular(10),
+      //                 topRight: Radius.circular(10),
+      //               ),
+      //             ),
+      //           ),
+      //           // Bagian Input Data Yang Sudah Di tentukan di Atas (TAHAP 2)
+      //           Container(
+      //             child: Text('${listBeforeSorting}'),
+      //             padding: EdgeInsets.all(10),
+      //             height: 130,
+      //             width: double.infinity,
+      //             decoration: BoxDecoration(
+      //               borderRadius: BorderRadius.only(
+      //                   bottomLeft: Radius.circular(10),
+      //                   bottomRight: Radius.circular(10)),
+      //               color: Colors.grey[350],
+      //             ),
+      //           ),
+      //           SizedBox(
+      //             height: 10,
+      //           ),
+      //           Container(
+      //             width: double.infinity,
+      //             child: FilledButton(
+      //               onPressed: () {
+      //                 List<int> listBeforeSorting = List.from(list);
+      //                 setState(() {
+      //                   sorting(list);
+      //                   // Menampilkan list sebelum diurutkan
+      //                   debugPrint('Sebelum sorting: ${listBeforeSorting}');
+      //                   // Menampilkan list setelah diurutkan
+      //                   debugPrint('Setelah sorting: ${list}');
+      //                 });
+      //               },
+      //               child: Text('URUTKAN'),
+      //             ),
+      //           ),
+      //           SizedBox(
+      //             height: 10,
+      //           ),
+      //           Container(
+      //             height: 30,
+      //             width: double.infinity,
+      //             child: Align(
+      //               alignment: Alignment.center,
+      //               child: Text(
+      //                 'DATA SETELAH DI SORTING',
+      //                 style: TextStyle(
+      //                   color: Colors.black,
+      //                   fontWeight: FontWeight.bold,
+      //                 ),
+      //               ),
+      //             ),
+      //             decoration: BoxDecoration(
+      //               color: Colors.green[200],
+      //               borderRadius: BorderRadius.only(
+      //                 topLeft: Radius.circular(10),
+      //                 topRight: Radius.circular(10),
+      //               ),
+      //             ),
+      //           ),
+      //           // Bagian Data Yang Sudah Di Tentukan (TAHAP 3)
+      //           Container(
+      //             child: Text('${list}'),
+      //             padding: EdgeInsets.all(10),
+      //             height: 130,
+      //             width: double.infinity,
+      //             decoration: BoxDecoration(
+      //               borderRadius: BorderRadius.only(
+      //                   bottomLeft: Radius.circular(10),
+      //                   bottomRight: Radius.circular(10)),
+      //               color: Colors.grey[350],
+      //             ),
+      //           ),
+      //         ],
+      //       ),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
